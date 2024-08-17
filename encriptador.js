dea8fef240a31ee28954bdab5eacbcb1e7551cf5
gabrielledezma21 <gabrielagustin.ledezma@estudiantes.unahur.edu.ar>
2024-08-17 04:57:44 -0300

    funcionalidades terminadas, falta el CSS

--- encriptador.js
+++ encriptador.js
@@ -5,115 +5,137 @@ function capturarTexto(){
     textoInicial = document.getElementById('encriptador__texto').value;
 }
 
-function encriptarTexto(){
+function estaVacio(cadenaDeCaracteres){
+    return cadenaDeCaracteres.trim().length === 0;
+}
+
+function validarTextoInicial(){
     capturarTexto();
-    textoFinal = "";
-    if(textoInicial === ""){
-        textoFinal = "Ingresa el texto que desees encriptar o desencriptar.";
-        
+    if(estaVacio(textoInicial)){
+        alert("Ingrese el texto que desea encriptar.");
+        //mostrarObjetos();
     } else {
-        for (let i = 0; i < textoInicial.length; i++) {
-            let letra = textoInicial[i];
-            switch (letra) {
-                case 'e':
-                    textoFinal += 'enter';
-                    break;
-                case 'i':
-                    textoFinal += 'imes';
-                    break;
-                case 'a':
-                    textoFinal += 'ai';
-                    break;
-                case 'o':
-                    textoFinal += 'ober';
-                    break;
-                case 'u':
-                    textoFinal += 'ufat';
-                    break;
-                default:
-                    textoFinal += letra;
-            }
-        }
+        //ocultarObjetos();
+        //mostrarResultado(textoFinal);
         
     }
-    mostrarResultado(textoFinal);
+    
+}
 
+function encriptarTexto(){
+    validarTextoInicial();
+    for (let i = 0; i < textoInicial.length; i++) {
+        let letra = textoInicial[i];
+        switch (letra) {
+            case 'e':
+                textoFinal += 'enter';
+                break;
+            case 'i':
+                textoFinal += 'imes';
+                break;
+            case 'a':
+                textoFinal += 'ai';
+                break;
+            case 'o':
+                textoFinal += 'ober';
+                break;
+            case 'u':
+                textoFinal += 'ufat';
+                break;
+            default:
+                textoFinal += letra;
+            }
+        }
+    mostrarResultado(textoFinal);
+    reestablecerValoresIniciales();
 }
 
 function desencriptarTexto(){
-    capturarTexto();
-    if (textoInicial == "") {
-        textoFinal = "Ingresa el texto que desees encriptar o desencriptar.";   
-        
-    } else {
-        textoFinal = "";
-        let i = 0;
-        while (i < textoInicial.length) {
-            if (textoInicial.substr(i, 5) === 'enter') {
-                textoFinal += 'e';
-                i += 5;
-            } else if (textoInicial.substr(i, 4) === 'imes') {
-                textoFinal += 'i';
-                i += 4;
-            } else if (textoInicial.substr(i, 2) === 'ai') {
-                textoFinal += 'a';
-                i += 2;
-            } else if (textoInicial.substr(i, 4) === 'ober') {
-                textoFinal += 'o';
-                i += 4;
-            } else if (textoInicial.substr(i, 4) === 'ufat') {
-                textoFinal += 'u';
-                i += 4;
-            } else {
-                textoFinal += textoInicial[i];
-                i++;
-            }
+    validarTextoInicial();
+    textoFinal = "";
+    let i = 0;
+    while (i < textoInicial.length) {
+        if (textoInicial.substr(i, 5) === 'enter') {
+            textoFinal += 'e';
+            i += 5;
+        } else if (textoInicial.substr(i, 4) === 'imes') {
+            textoFinal += 'i';
+            i += 4;
+        } else if (textoInicial.substr(i, 2) === 'ai') {
+            textoFinal += 'a';
+            i += 2;
+        } else if (textoInicial.substr(i, 4) === 'ober') {
+            textoFinal += 'o';
+            i += 4;
+        } else if (textoInicial.substr(i, 4) === 'ufat') {
+            textoFinal += 'u';
+            i += 4;
+        } else {
+            textoFinal += textoInicial[i];
+            i++;
         }
-        
     }
     mostrarResultado(textoFinal);
+    reestablecerValoresIniciales();
 }
 
 function mostrarResultado(texto){
-    document.getElementById('resultado__texto').innerText = texto;
+    document.getElementById('resultado__obtenido').innerText = texto;
+    ocultarObjetos();
+}
+
+function reestablecerValoresIniciales(){
     textoFinal = "";
     textoInicial = "";
 }
 
 function copiarTexto() {
-    let textoParaCopiar = document.getElementById('resultado__texto').innerText;
-
-    if (!navigator.clipboard) {
-        // Fallback para navegadores que no soportan navigator.clipboard
-        let textArea = document.createElement("textarea");
-        textArea.value = textoParaCopiar;
-        document.body.appendChild(textArea);
-        textArea.focus();
-        textArea.select();
-        try {
-            document.execCommand('copy');
-        } catch (err) {
-            alert('Oops, no se pudo copiar el texto');
-        }
-        document.body.removeChild(textArea);
-    } else {
-        navigator.clipboard.writeText(textoParaCopiar).then(() => {
-        }).catch(err => {
-            alert('Oops, no se pudo copiar el texto');
-        });
+    let textoParaCopiar = document.getElementById('resultado__obtenido').innerText;
+    let textArea = document.createElement("textarea");
+    textArea.value = textoParaCopiar;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+        document.execCommand('copy');
+    } catch (err) {
+        alert('No se pudo copiar el texto');
     }
-}
-
-function ocultarObjetos(){
-    document.getElementById('resultado__imagen').hidden = true;
-    document.getElementById('resultado__vacio').innerHTML = "";
-    document.getElementById('resultado__copiar').hidden = false;
+    document.body.removeChild(textArea);
 }
 
 function mostrarObjetos(){
-    document.getElementById('resultado__imagen').hidden = false;
-    document.getElementById('resultado__vacio').innerHTML = "Ningún mensaje fue encontrado";
-    document.getElementById('resultado__copiar').hidden = true;
+    mostrarObjeto("resultado__imagen");
+    mostrarObjeto("resultado__vacio");
+    mostrarObjeto("resultado__texto");
+    ocultarObjeto("resultado__copiar");
+    ocultarObjeto("resultado__obtenido");
+}
+
+function ocultarObjetos(){
+    ocultarObjeto("resultado__imagen");
+    ocultarObjeto("resultado__vacio");
+    ocultarObjeto("resultado__texto");
+    mostrarObjeto("resultado__copiar");
+    mostrarObjeto("resultado__obtenido");
+}
+
+function mostrarObjeto(id) {
+    const elemento = document.getElementById(id);
+    if (elemento) {
+        elemento.hidden = false;
+    } else {
+        console.error(`Elemento con ID ${id} no encontrado.`);
+    }
+}
+
+function ocultarObjeto(id) {
+    const elemento = document.getElementById(id);
+    if (elemento) {
+        elemento.style.display = 'none';
+    } else {
+        console.error(`Elemento con ID ${id} no encontrado.`);
+    }
 }
 
 document.getElementById('encriptador__actuar').addEventListener('click', encriptarTexto);
